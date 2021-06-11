e6567b4d4db92a6f2f56530301efda85c12e30ec
EmergencyPlayer <EmergencyPlayer@users.noreply.github.com>
2021-06-11 13:34:34 -0700

    Added Shooting
    Added shooting

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -7,7 +7,7 @@ public class Shooting : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
 
-    publoc float bulletForce = 20f;
+    public float bulletForce = 20f;
 
     // Update is called once per frame
     void Update()
@@ -16,5 +16,12 @@ public class Shooting : MonoBehaviour
         {
             Shoot();
         }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
     }
 }
