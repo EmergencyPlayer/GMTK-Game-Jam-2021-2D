8423648b45d84d102d0264e51cc0b1a0de5fb959
thblaauw <thblaauw@gmail.com>
2021-06-12 11:22:33 -0700

    No more GlueLine

--- Assets/Scripts/Items/Gluable/Gluable.cs
+++ Assets/Scripts/Items/Gluable/Gluable.cs
@@ -12,8 +12,6 @@ public class Gluable : MonoBehaviour
     private GameObject target;
     private bool isParent;
 
-    [SerializeField] private GameObject line;
-    private GlueLine gl;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +19,7 @@ public class Gluable : MonoBehaviour
         this.rb = gameObject.GetComponent<Rigidbody2D>();
         this.isGlueing = false;
         this.isParent = false;
-        this.gl = line.GetComponent<GlueLine>();
+        
     }
 
     // Update is called once per frame
@@ -51,13 +49,7 @@ public class Gluable : MonoBehaviour
         this.isParent = !flag;
         target = g;
         isGlueing = true;
-        g.GetComponent<Gluable>().GlueTo(gameObject, !flag);
-
-        if (this.isParent)
-        {
-            line.SetActive(true);
-            gl.MakeLine(g);
-        }
+        g.GetComponent<Gluable>().GlueTo(gameObject, !flag);      
       
     }
 
@@ -71,8 +63,6 @@ public class Gluable : MonoBehaviour
         rb.angularVelocity = 0.0f;
         rb.gravityScale = 1f;
         isGlueing = false;
-        gl.StopLine();
-        line.SetActive(false);
 
         if (!isParent)
         {
