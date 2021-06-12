e8f34d1e9db85597f1c5f8551670517bd64c99f9
EmergencyPlayer <EmergencyPlayer@users.noreply.github.com>
2021-06-12 14:29:57 -0700

    Finished 4th Level
    Finished 3 and 4th level along with adjustments

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -6,6 +6,7 @@ public class Character2D : MonoBehaviour
     public float JumpForce = 1;
 
     private Rigidbody2D _rigidbody;
+    public Animator anim;
 
     private void Start()
     {
@@ -19,10 +20,20 @@ public class Character2D : MonoBehaviour
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
         if (!Mathf.Approximately(0, movement))
+        {
             transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }
+
         if(Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
-            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse); 
+            anim.SetBool("isJumping", true);
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            anim.SetBool("isJumping", false);
         }
+        else
+        {
+            //anim.SetBool("isJumping", false);
+        }
+        anim.SetFloat("speed", Input.GetAxis("Horizontal"));
     }
 }
