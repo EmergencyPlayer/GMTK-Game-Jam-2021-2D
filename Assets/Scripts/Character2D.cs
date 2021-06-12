e8c184e26a972efe6a9278dec0175c92b10bffbf
Haru1829 <skylor.yoon@gmail.com>
2021-06-12 14:53:46 -0700

    Update
    Update Sound

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -20,6 +20,8 @@ public class Character2D : MonoBehaviour
 
         if (!Mathf.Approximately(0, movement))
             transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+           
+        
         if(Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse); 
