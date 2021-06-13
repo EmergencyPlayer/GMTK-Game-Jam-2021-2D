fcdc2111c02b65df16b15cf3e951db5a0d2f8b09
EmergencyPlayer <EmergencyPlayer@users.noreply.github.com>
2021-06-12 21:06:30 -0700

    Fixed Starting menu
    Starting Menu si fully finished

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Level4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Level5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Level6()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void Level7()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void Level8()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void Level9()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void Level10()
+    {
+        SceneManager.LoadScene(10);
+    }
+    public void Level11()
+    {
+        SceneManager.LoadScene(11);
+    }
+    public void Level12()
+    {
+        SceneManager.LoadScene(12);
+    }
+
+}
